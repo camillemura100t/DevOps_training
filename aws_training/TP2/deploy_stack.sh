fa33f1a0492fd37f4a4ea9c3e94f43887ab14aa2
user.email <camille.mura100t@gmail.com>
2021-06-18 12:59:29 +0000

    MAJ scripts

--- aws_training/TP2/deploy_stack.sh
+++ aws_training/TP2/deploy_stack.sh
@@ -1,67 +1,74 @@
 #!/bin/bash
 
+# variables
+
 KEY_NAME=Tp2KeyPair
 PATH_KEY=~/.ssh/Tp2KeyPair.pem
 STACK_NAME=CamilleTp2Stack
-TPL=stack-tpl.yaml
-TPL_UPD=stack-tpl-upd.yaml
+STACK_TPL=stack-tpl.yaml
+STACK_TPL_UPD=stack-tpl-upd.yaml
 INS_TYPE=t2.micro
 IMG_ID=ami-00c08ad1a6ca8ca7c
 BUCKET_NAME=camille-s3-bucket
 MY_PUB_CIDR=77.131.74.214/32
 
-# # create ssh key pair
+# functions
 
-# echo 'Check if Key exists on AWS / locally'
+input_stack_parameters () {
+echo 'Enter a stack name (no spaces - ex: CamilleTp2Stack)'
+read STACK_NAME
+echo 'Enter the instance type (official AWS type - ex: t2.micro)'
+read INS_TYPE
+echo 'Enter the AMI (image ID - ex: ami-00c08ad1a6ca8ca7c)'
+read IMG_ID
+echo 'Enter your ssh location (public IP - CIDR format - ex: 123.58.13.2/32)'
+read MY_PUB_CIDR
+echo 'Enter the S3 bucket name (3-63 characters - lowercase letters, numbers, dots and hyphens only - ex: c.m-s3-bucket)'
+read BUCKET_NAME
+}
 
-# if aws ec2 wait key-pair-exists --key-names $KEY_NAME
-#     then
-#     echo 'Key already exists on AWS, deleting it'
-#     aws ec2 delete-key-pair --key-name $KEY_NAME
-# fi
+# create ssh key pair
 
-# if test -f $PATH_KEY
-#     then
-#     echo 'Key already exists locally, deleting it'
-#     sudo rm -f $PATH_KEY
-# fi
+echo 'Check if Key exists on AWS / locally'
 
-# echo 'OK, key does not exists on both sides'
+if aws ec2 wait key-pair-exists --key-names $KEY_NAME
+    then
+    echo 'Key already exists on AWS, deleting it'
+    aws ec2 delete-key-pair --key-name $KEY_NAME
+fi
 
-# aws ec2 create-key-pair \
-#     --key-name $KEY_NAME \
-#     --query 'KeyMaterial' \
-#     --output text > $PATH_KEY
+if test -f $PATH_KEY
+    then
+    echo 'Key already exists locally, deleting it'
+    sudo rm -f $PATH_KEY
+fi
 
-# # modify rights on ssh key pair
+echo 'OK, key does not exists on both sides'
 
-# sleep 1
-# chmod 400 $PATH_KEY
+aws ec2 create-key-pair \
+    --key-name $KEY_NAME \
+    --query 'KeyMaterial' \
+    --output text > $PATH_KEY
 
-# echo 'SSH created & ready to be used'
+# modify rights on ssh key pair
+
+sleep 1
+chmod 400 $PATH_KEY
+
+echo 'SSH created & ready to be used'
 
 echo -e "Enter \"create\" to create a new stack\nEnter \"update\" to update an existing stack\nEnter \"delete\" to delete an existing stack"
 
 read CHOICE
 
-echo 'Enter a stack name'
-read STACK_NAME
-echo 'Enter the template file name (*.yaml, *.json)'
-read TPL
-echo 'Enter the instance type (t2.micro...)'
-read INS_TYPE
-echo 'Enter the AMI (image ID)'
-read IMG_ID
-echo 'Enter your ssh location (public IP)'
-read MY_PUB_CIDR
-echo 'Enter the S3 bucket name'
-read BUCKET_NAME
-
 if [ $CHOICE = "create" ]
     then
+        input_stack_parameters
+        echo 'Enter the template file name (*.yaml, *.json - must be in you current folder)'
+        read STACK_TPL
         aws cloudformation create-stack \
         --stack-name $STACK_NAME \
-        --template-body file://$TPL \
+        --template-body file://$STACK_TPL \
         --parameters ParameterKey=InstanceType,ParameterValue=$INS_TYPE \
         ParameterKey=ImageId,ParameterValue=$IMG_ID \
         ParameterKey=KeyName,ParameterValue=$KEY_NAME \
@@ -72,9 +79,12 @@ fi
 
 if [ $CHOICE = "update" ]
     then
+        input_stack_parameters
+        echo 'Enter the updated template file name (*.yaml, *.json - must be in you current folder)'
+        read STACK_TPL_UPD
         aws cloudformation update-stack \
         --stack-name $STACK_NAME \
-        --template-body file://$TPL_UPD \
+        --template-body file://$STACK_TPL_UPD \
         --parameters ParameterKey=InstanceType,ParameterValue=$INS_TYPE \
         ParameterKey=ImageId,ParameterValue=$IMG_ID \
         ParameterKey=KeyName,ParameterValue=$KEY_NAME \
@@ -85,6 +95,8 @@ fi
 
 if [ $CHOICE = "delete" ]
     then
+        echo 'Enter a stack name'
+        read STACK_NAME
         aws cloudformation delete-stack \
         --stack-name $STACK_NAME
         echo 'Deleting stack...'
